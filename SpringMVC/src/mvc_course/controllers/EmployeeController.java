04b740142eea80bdd0c2624dc76b225d21c7ae48
Ciara <C.Magee@kainos.com>
2016-10-21 09:40:46 +0100

    EmployeeController class

---
+++ SpringMVC/src/mvc_course/controllers/EmployeeController.java
@@ -0,0 +1,60 @@
+package mvc_course.controllers;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import mvc_course.models.Employee;
+
+
+@Controller
+public class EmployeeController{
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@RequestMapping(value="addEmployee.mvc")
+	public String addEmployee(){
+		return "addEmployee";
+	}
+		
+	  @RequestMapping(value = "submitForm.mvc")
+	  public String addEmployee(@ModelAttribute("employee") Employee e) {
+		  try{
+		  Connection c = dataSource.getConnection();
+		  PreparedStatement ps = c.prepareStatement("INSERT INTO Employees" + "(name, Address, National_insurance_number, Bank_account_IBAN, Starting_salary, Employee_type_id)" + "VALUES (?,?,?,?,?,?)");	        
+		  ps.setString(1, e.getName());
+		  ps.setString(2,  e.getAddress());
+		  ps.setString(3,  e.getNi_number());
+		  ps.setString(4, e.getIban_number());
+		  ps.setDouble(5,  e.getStarting_salary());
+		  ps.setInt(6,  1);
+		  ps.executeUpdate();
+		  }catch(Exception ex){
+			  System.out.println(ex.getMessage());
+		  }
+		  return "redirect:showEmployees.mvc";
+	  }	
+	  
+	  @RequestMapping(value ="showEmployees.mvc")
+		public ModelAndView showEmployees(ModelAndView mav){
+			mav.setViewName("employee");
+			mav.addObject("showEmployees", Employee.employeeList);
+			return mav;
+	  }
+}
