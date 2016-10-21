156ff8713610004abbd1c3b9746c26f6684f7db2
Ciara <C.Magee@kainos.com>
2016-10-21 09:53:53 +0100

    ciaras new branch

--- SpringMVC/src/mvc_course/controllers/EmployeeController.java
+++ SpringMVC/src/mvc_course/controllers/EmployeeController.java
@@ -2,23 +2,15 @@ package mvc_course.controllers;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
 import mvc_course.models.Employee;
 
 
